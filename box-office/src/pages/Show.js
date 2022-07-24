98001718132171968825bdf0f16dcda0d4e6f08e
jyotisahu <standon81149@gmail.com>
2022-07-24 18:50:18 +0530

    styled show page

--- box-office/src/pages/Show.js
+++ box-office/src/pages/Show.js
@@ -5,8 +5,8 @@ import Cast from '../component/show/Cast';
 import Details from '../component/show/Details';
 import Seasons from '../component/show/Seasons';
 import ShowMainData from '../component/show/ShowMainData';
-
 import { apiGet } from '../misc/config';
+import { ShowPageWrapper, InfoBlock } from './Show.styled';
 
 const reducer = (prevState, action) => {
   switch (action.type) {
@@ -66,7 +66,7 @@ const Show = () => {
   }
 
   return (
-    <div>
+    <ShowPageWrapper>
       <ShowMainData
         image={show.image}
         name={show.name}
@@ -75,25 +75,25 @@ const Show = () => {
         tags={show.genres}
       />
 
-      <div>
+      <InfoBlock>
         <h2>Details</h2>
         <Details
           status={show.status}
           network={show.network}
           premiered={show.premiered}
         />
-      </div>
+      </InfoBlock>
 
-      <div>
+      <InfoBlock>
         <h2>Seasons</h2>
         <Seasons seasons={show._embedded.seasons} />
-      </div>
+      </InfoBlock>
 
-      <div>
+      <InfoBlock>
         <h2>Cast</h2>
         <Cast cast={show._embedded.cast} />
-      </div>
-    </div>
+      </InfoBlock>
+    </ShowPageWrapper>
   );
 };
 
